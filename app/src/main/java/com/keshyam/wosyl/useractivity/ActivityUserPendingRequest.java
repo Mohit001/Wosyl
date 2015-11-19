d29fd623ff4b720b177b3af9ddc53d99c2c7f75c
Mohit001 <gandhi.mohit001@gmail.com>
2015-11-19 09:50:56 +0530

    11_19_2015_0950am
    pending request detail page on working

---
+++ app/src/main/java/com/keshyam/wosyl/useractivity/ActivityUserPendingRequest.java
@@ -0,0 +1,9 @@
+package com.keshyam.wosyl.useractivity;
+
+/**
+ * Created by Liger on 11/19/2015.
+ */
+public class ActivityUserPendingRequest extends ActivityNewRide
+{
+
+}
---
+++ app/src/main/java/com/keshyam/wosyl/userfragment/UserPendingDetailsFragment.java
@@ -0,0 +1,10 @@
+package com.keshyam.wosyl.userfragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by Liger on 11/19/2015.
+ */
+public class UserPendingDetailsFragment extends Fragment
+{
+}
